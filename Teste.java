3a291aaed39719cfb2c4f5d82359b2e8c2171862
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2018-07-06 10:24:45 -0300

    Classe inical de manipulação de solicitante do acesso

---
+++ Teste.java
@@ -0,0 +1,11 @@
+import solicitante.Solicitante;
+
+public class Teste{
+	
+	public static void main(String args[]){
+
+		Solicitante s1 = new Solicitante("20151121018","maxsuel","medeiros","123456");
+		System.out.println(s1.getMatricula());
+
+	}
+}
