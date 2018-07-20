f9b716319b66035b90ea438cd4f0570dc2536919
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2018-07-20 09:54:02 -0300

    adicionando com um projeto no eclipse

--- JDBCExemplo.java
+++ JDBCExemplo.java
@@ -1,21 +1,43 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
  
 class JDBCExemplo {
  
-    private static final String url = "jdbc:mysql://192.168.25.37/3306/Projeto_PAD";
+    private static final String url = "jdbc:mysql://192.168.25.37/projeto_pad";
  
-    private static final String user = "root";
+    private static final String user = "PAD";
  
-    private static final String password = "novasenha";
+    private static final String password = "projeto";
+
+   private static final String driver = "com.mysql.cj.jdbc.Driver";
  
     public static void main(String args[]) {
+        Connection conn = null;
+         
         try {
-            Connection con = DriverManager.getConnection(url, user, password);
-            System.out.println("Success");
- 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            Class.forName(driver).newInstance();
+    conn = DriverManager.getConnection("jdbc:mysql://192.168.25.37/projeto_pad" + "user=PAD&password=projeto"  );
+    System.out.println("sucesso");
+
+
+    // Do something with the Connection
+
+
+} catch (SQLException ex) {
+    // handle any errors
+    System.out.println("SQLException: " + ex.getMessage());
+    System.out.println("SQLState: " + ex.getSQLState());
+    System.out.println("VendorError: " + ex.getErrorCode());
+}catch (Exception ex){
+    System.out.println("ERRO: " + ex.getMessage());
+
+}
     }
 }
+
+
+
