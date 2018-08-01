fd6ff3c1433dde0098c6d2a8747325d3ae395491
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2018-08-01 13:28:59 -0300

    adicionando a classe que vai ser o triguer do projeto

---
+++ APP.java
@@ -0,0 +1,10 @@
+/*Essa classe vai ser a responsável por executar as funções mais importantes
+ * do projeto, dessa forma, executando as classes criadas
+ */
+public class APP {
+	
+	public static void main (String args[]) {
+		
+	}
+
+}
